05a13f7e56b09db888ea4770888452c7da966937
Davide Perugini <davide.perugini@mail.polimi.it>
2020-05-10 15:44:33 +0200

    Added functions in movable

--- Assets/Scripts/Mechanics/Movable.cs
+++ Assets/Scripts/Mechanics/Movable.cs
@@ -8,8 +8,12 @@ namespace Mechanics
         protected Transform Tr;
         protected Rigidbody2D Rb;
         protected Vector2 TargetPosition;
-        protected bool IsMoving;
         protected bool MoveFixedDistance;
+        protected bool MoveFixedDistanceAccelerated;
+        private float _fixedSpeed;
+        private float _fixedDistance;
+        private Vector2 _fixedDirection;
+        private float _fixedAcceleration;
 
         private void Awake()
         {
@@ -27,10 +31,39 @@ namespace Mechanics
             Tr.position = (Vector2)Tr.position + Time.deltaTime * speed * direction;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void SetFixedDistance(Vector2 direction, float speed, float distance)
         {
+            MoveFixedDistance = true;
+            _fixedDirection = direction;
+            _fixedDistance = distance;
+            _fixedSpeed = speed;
+        }
         
+        public void SetFixedDistanceAccelerated(Vector2 direction, float initSpeed, float distance, float acceleration)
+        {
+            MoveFixedDistanceAccelerated = true;
+            _fixedDirection = direction;
+            _fixedDistance = distance;
+            _fixedSpeed = initSpeed;
+            _fixedAcceleration = acceleration;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (MoveFixedDistance)
+            {
+                Tr.position = (Vector2)Tr.position + Time.deltaTime * _fixedSpeed * _fixedDirection;
+                _fixedDistance = _fixedDistance - Time.deltaTime * _fixedSpeed;
+                if(_fixedDistance <= 0) MoveFixedDistance = false;
+            }
+
+            if (MoveFixedDistanceAccelerated)
+            {
+                Tr.position = (Vector2)Tr.position + Time.deltaTime * _fixedSpeed * _fixedDirection + .5f * Time.deltaTime * _fixedAcceleration * _fixedDirection;
+                _fixedDistance = _fixedDistance - Time.deltaTime * _fixedSpeed - .5f * Time.deltaTime * _fixedAcceleration;
+                if(_fixedDistance <= 0) MoveFixedDistanceAccelerated = false;
+            }
         }
     }
 }
