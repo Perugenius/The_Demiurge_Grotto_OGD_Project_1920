967c5ffb781b3bc95b2115e723e5d76c9835ad8e
Davide Perugini <davide.perugini@mail.polimi.it>
2020-05-11 19:02:58 +0200

    Mushroom enemy

--- Assets/Scripts/Mechanics/Enemies/Mushroom.cs
+++ Assets/Scripts/Mechanics/Enemies/Mushroom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Mechanics.Enemies
@@ -8,14 +9,20 @@ namespace Mechanics.Enemies
     public class Mushroom : Movable
     {
         private Vector2 _direction;
+        [SerializeField] private float speed;
+        [SerializeField] private bool initialDirection;
         
         
         // Start is called before the first frame update
         void Start()
         {
-            _direction = Random.Range(0, 1) == 0
+            _direction = initialDirection
                 ? Vector2.left
                 : Vector2.right;
+            
+            Vector3 scale = Tr.localScale;
+            scale.x = -_direction.x;
+            Tr.localScale = scale;
         }
 
         private void Update()
@@ -26,12 +33,13 @@ namespace Mechanics.Enemies
         protected override void FixedUpdate()
         {
             base.FixedUpdate();
-            MoveDynamic(_direction * Thrust);
-            Debug.Log(Physics2D.OverlapPoint(Tr.position + new Vector3(_direction.x * 2, -1, 0), LayerMask.GetMask("Obstacle")));
-            if (!Physics2D.OverlapPoint(Tr.position + new Vector3(_direction.x * 2, -1, 0), LayerMask.GetMask("Obstacle")))
+            MoveDynamic(_direction, speed);
+            if (!Physics2D.OverlapPoint(Tr.position + new Vector3(_direction.x, -1, 0), LayerMask.GetMask("Obstacle")) || Physics2D.OverlapPoint(Tr.position + new Vector3(_direction.x, 0, 0), LayerMask.GetMask("Obstacle")))
             {
-                _direction = Vector2.Reflect(_direction, Vector2.up);
-                Debug.Log(_direction);
+                _direction = Vector2.Reflect(_direction, Vector2.right);
+                Vector3 newScale = Tr.localScale;
+                newScale.x = -_direction.x;
+                Tr.localScale = newScale;
             }
         }
     }
