90c82ca9fde8fae6af68c4fd954fc5e7f22ac285
Elio <elio.salvini@mail.polimi.it>
2020-05-12 19:19:44 +0200

    CameraTesting

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : Singleton<GameManager>
         if (gameObject.GetPhotonView().IsMine)
         {
             GameObject player = PhotonNetwork.Instantiate("VoodooTmp", new Vector3(-20f, 0f, 0f), Quaternion.identity);
-            GameObject myCamera = Instantiate(camera, new Vector3(-20f,0f,-10f), Quaternion.identity);
+            GameObject myCamera = GameObject.Find("Main Camera");
             myCamera.GetComponent<CameraFocusOnPlayer>().cameraPlayer = player;
         }
     }
