45cd4407607c6808bc1b5ac2f0217616e909e762
Elio <elio.salvini@mail.polimi.it>
2020-05-27 19:29:26 +0200

    Destroyable chest

---
+++ Assets/Chest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private static readonly int IsHit = Animator.StringToHash("isHit");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //TODO check if player has the right skill to break the box
+        PhotonView photonView = PhotonView.Get(this);
+        if(other.gameObject.GetPhotonView().IsMine) photonView.RPC("DestroyChest", RpcTarget.All);
+    }
+    
+    [PunRPC]
+    private void DestroyChest()
+    {
+        animator.SetBool(IsHit, true);
+        StartCoroutine(WaitAnimationEnd());
+    }
+
+    private IEnumerator WaitAnimationEnd()
+    {
+        yield return new WaitForSeconds(3f/20f);
+        gameObject.transform.parent.Find("Pieces").gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    
+    
+}
---
+++ Assets/ChestPiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mechanics;
+using UnityEngine;
+
+public class ChestPiece : MonoBehaviour
+{
+    [SerializeField] private Vector2 impulseDirection;
+    [SerializeField] private float impulseIntensity = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(rb.velocity.x,0);
+        rb.AddForce(impulseDirection.normalized * impulseIntensity,ForceMode2D.Impulse);
+        StartCoroutine(WaitBeforeDestroying());
+    }
+
+    private IEnumerator WaitBeforeDestroying()
+    {
+        yield return new WaitForSeconds(30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
