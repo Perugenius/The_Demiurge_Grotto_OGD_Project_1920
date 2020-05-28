0f1f6831cc43a7ad6ede6e970c9d8d27eb317729
Panicch <37112713+Panicch@users.noreply.github.com>
2020-05-28 20:07:24 +0200

    Update PlayableCharacter.cs

--- Assets/Scripts/Mechanics/Players/PlayableCharacter.cs
+++ Assets/Scripts/Mechanics/Players/PlayableCharacter.cs
@@ -43,8 +43,8 @@ namespace Mechanics.Players
                 if (Input.GetButtonDown("Jump"))
                 {
                     RaycastHit2D raycast = Physics2D.Raycast(Tr.position,Vector2.down, 1.1f,LayerMask.GetMask("Obstacle"));
-                    RaycastHit2D playerRaycastHit2D = Physics2D.Raycast(Tr.position,Vector2.down, 1.2f,LayerMask.GetMask("PlayerPhysic"));
-                    if (raycast || (playerRaycastHit2D && playerRaycastHit2D.collider != gameObject.GetComponent<Collider2D>()))
+                    RaycastHit2D playerRaycastHit2D = Physics2D.Raycast(new Vector2(Tr.position.x,Tr.position.y-1.1f), Vector2.down, 0.5f,LayerMask.GetMask("PlayerPhysic"));
+                    if (raycast || playerRaycastHit2D)
                     {
                         IsJumping = true;
                         Jump(48);
