525e7a43daa16875a457dfd9aa3aa7793f4acac5
Elio <elio.salvini@mail.polimi.it>
2020-08-07 20:30:33 +0200

    Main Mneu screen resolution testing

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -25,7 +25,6 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetResolution();
         _titleMenu = title.GetComponent<Menu>();
         _hillsMenu = backgroundHills.GetComponent<Menu>();
         _homeMenu = home.GetComponent<Menu>();
@@ -33,12 +32,6 @@ public class MainMenu : MonoBehaviour
         _perksMenu = perksShop.GetComponent<Menu>();
         _charactersMenu = characters.GetComponent<Menu>();
         _creditsMenu = credits.GetComponent<Menu>();
-    }
-
-    private void SetResolution()
-    {
-        float scale = UnityEngine.Screen.dpi / 96f;
-        transform.Find("Canvas").gameObject.GetComponent<CanvasScaler>().scaleFactor = scale;
     }
 
     // Update is called once per frame
