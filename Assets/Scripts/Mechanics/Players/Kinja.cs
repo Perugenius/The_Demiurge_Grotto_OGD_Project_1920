1819505c14e5d286b0cb5f7ebf721550748a6985
Panicch <37112713+Panicch@users.noreply.github.com>
2020-08-10 17:48:17 +0200

    Update Kinja

--- Assets/Scripts/Mechanics/Players/Kinja.cs
+++ Assets/Scripts/Mechanics/Players/Kinja.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using Mechanics.Players.PlayerAttacks;
+using Photon.Pun;
+using UnityEngine;
 
 namespace Mechanics.Players
 {
@@ -7,12 +10,14 @@ namespace Mechanics.Players
 
         private int _maxJumpsNumber;
         private int _jumpsNumber;
+        private Transform _attackSpawner;
         
         // Start is called before the first frame update
         protected override void Start()
         {
             base.Start();
             _maxJumpsNumber = statistics.maxJumps;
+            _attackSpawner = transform.Find("AttackPoint").transform;
         }
 
         // Update is called once per frame
@@ -20,9 +25,14 @@ namespace Mechanics.Players
         {
             if (Input.GetButtonDown("Jump") && _jumpsNumber < _maxJumpsNumber && IsJumping)
             {
-                Jump(62);
+                Jump(45);
                 _jumpsNumber++;
                 Animator.SetBool(IsJumpingAnim, true);
+            }
+
+            if (Input.GetButtonDown("Attack") && IsJumping)
+            {
+                Attack();
             }
             base.Update();
         }
@@ -39,7 +49,10 @@ namespace Mechanics.Players
 
         protected override void Attack()
         {
-            throw new System.NotImplementedException();
+            GameObject shuriken = PhotonNetwork.Instantiate(Path.Combine("Players", "Shuriken"), _attackSpawner.position, Quaternion.identity);
+            Shuriken shurikenScript = shuriken.GetComponent<Shuriken>();
+            shurikenScript.SetDamage(CurrentAttack);
+            shurikenScript.SetSpeed(Vector2.down*12);
         }
     }
 }
---
+++ Assets/Scripts/Mechanics/Players/PlayerAttacks/Shuriken.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mechanics.Players.PlayerAttacks
+{
+    public class Shuriken : MonoBehaviour
+    {
+
+        private Rigidbody2D _rb;
+        private float _damage;
+        private Vector2 _speed;
+    
+    
+    
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rb = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position += (Vector3) (Time.deltaTime * _speed);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        { 
+            Debug.Log(other.gameObject.name);
+            Destroy(gameObject);
+        }
+
+        public void SetDamage(float damage)
+        {
+            _damage = damage;
+        }
+
+        public void SetSpeed(Vector2 speed)
+        {
+            _speed = speed;
+        }
+    
+    }
+}
