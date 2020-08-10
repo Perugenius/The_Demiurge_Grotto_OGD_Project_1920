bf2a7dacc5a0f4bd65ff2b07745a5758bcc06c7b
Elio <elio.salvini@mail.polimi.it>
2020-08-10 14:57:07 +0200

    DefenseBubble (EldaanLetter)
    To avoid a player to end the dungeon alone (leaving back the other player), a bubble prevents players from collecting eldaan's letters if they aren't both near to it.

---
+++ Assets/DefenseBubble.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseBubble : MonoBehaviour
+{
+    private List<GameObject> _players;
+    [SerializeField] private GameObject letter;
+    private const int _numOfPLayers = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player") && !_players.Contains(other.gameObject))
+        {
+            _players.Add(other.gameObject);
+        }
+
+        if (_players.Count == _numOfPLayers)
+        {
+            letter.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player") && _players.Contains(other.gameObject))
+        {
+            _players.Remove(other.gameObject);
+        }
+    }
+}
