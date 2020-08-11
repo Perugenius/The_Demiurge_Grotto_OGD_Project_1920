30367573d579de96f6bd5bbffdf832983693a4ca
Panicch <37112713+Panicch@users.noreply.github.com>
2020-08-11 18:13:12 +0200

    Added Pinkie

---
+++ Assets/Scripts/Mechanics/Players/Pinkie.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Mechanics.Players
+{
+    public class Pinkie : PlayableCharacter
+    {
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+            if (Input.GetButtonDown("Attack"))
+            {
+                Attack();
+            }
+        }
+
+        protected override void Die()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Attack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
