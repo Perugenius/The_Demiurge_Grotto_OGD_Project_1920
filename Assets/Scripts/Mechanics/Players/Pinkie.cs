4372b663d58930d907b2ed7e5178ff32b1bdd8e4
Panicch <37112713+Panicch@users.noreply.github.com>
2020-08-12 00:46:56 +0200

    Update Pinkie

--- Assets/Scripts/Mechanics/Players/Pinkie.cs
+++ Assets/Scripts/Mechanics/Players/Pinkie.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections;
+using System.IO;
 using Mechanics.Players.PlayerAttacks;
 using Photon.Pun;
 using UnityEngine;
@@ -11,12 +12,22 @@ namespace Mechanics.Players
 
         private bool _canSummonPillow = true;
         private int _maxPillowNumber;
+
+        private bool _canDisappear = true;
+        private SpriteRenderer _renderer;
+        private float _invincibilityDuration;
+        private float _timeLapse;
+        private GameObject _hitBox;
+        
         
         // Start is called before the first frame update
         protected override void Start()
         {
             base.Start();
             _pillowSpawnPosition = transform.Find("PillowSpawner").transform;
+            _renderer = GetComponent<SpriteRenderer>();
+            _hitBox = transform.Find("PlayerHitbox").gameObject;
+            _timeLapse = 4;
         }
 
         // Update is called once per frame
@@ -26,6 +37,11 @@ namespace Mechanics.Players
             if (Input.GetButtonDown("Attack") && _canSummonPillow)
             {
                 Attack();
+            }
+
+            if (Input.GetButtonDown("SecondarySkill") && _canDisappear)
+            {
+                UseSecondary();
             }
         }
 
@@ -51,6 +67,36 @@ namespace Mechanics.Players
         public void SetCanSummonPillow(bool canSummon)
         {
             _canSummonPillow = canSummon;
+        }
+
+        private void UseSecondary()
+        {
+            _canDisappear = false;
+            _renderer.color = new Color(255,255,255,0.5f);
+            _hitBox.SetActive(false);
+            StartCoroutine(nameof(InvincibilityTime));
+        }
+
+        private IEnumerator InvincibilityTime()
+        {
+            yield return new WaitForSeconds(4);
+            for (int i = 0; i <= 4; i++)
+            {
+                _renderer.color = new Color(255,255,255, 0.8f);
+                yield return new WaitForSeconds(0.25f);
+                _renderer.color = new Color(255,255,255, 0.5f);
+                yield return new WaitForSeconds(0.25f);
+            }
+            _renderer.color = new Color(255,255,255,1);
+            _hitBox.SetActive(true);
+            StartCoroutine(nameof(TimeLapse));
+
+        }
+
+        private IEnumerator TimeLapse()
+        {
+            yield return  new WaitForSeconds(_timeLapse);
+            _canDisappear = true;
         }
     }
 }
