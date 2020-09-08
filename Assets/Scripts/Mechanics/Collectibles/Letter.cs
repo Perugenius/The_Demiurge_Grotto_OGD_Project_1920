148a2352760af6069b62e94e72bbe68d71596032
Panicch <37112713+Panicch@users.noreply.github.com>
2020-09-08 17:56:19 +0200

    update audio

--- Assets/Scripts/Mechanics/Collectibles/Letter.cs
+++ Assets/Scripts/Mechanics/Collectibles/Letter.cs
@@ -91,7 +91,7 @@ namespace Mechanics.Collectibles
                 _collectiblesManager.CollectTeammateLetter();
                 transform.localScale = new Vector3(1,1,1);
                 _animator.SetBool(IsCollected, true);
-                AudioManager.Instance.PlaySound("TeammateLetterSFX");
+                AudioManager.Instance.PlaySound("TeamLetterSFX");
                 if(!offlineMode) GameManager.Instance.ShowMessage("New companion's letter found!");
                 StartCoroutine(WaitBeforeDestroy());
             }
