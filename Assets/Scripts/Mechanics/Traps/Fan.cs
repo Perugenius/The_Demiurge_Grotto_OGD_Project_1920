0b17ba43f827eb5393c8774658b014bbc6f5db42
Elio <elio.salvini@mail.polimi.it>
2020-09-08 16:07:54 +0200

    Fans disabled for testing

--- Assets/Scripts/Mechanics/Traps/Fan.cs
+++ Assets/Scripts/Mechanics/Traps/Fan.cs
@@ -125,7 +125,9 @@ public class Fan : MonoBehaviour
         // Update is called once per frame
     void FixedUpdate()
     {
-        if(Vector3.Distance(_player.transform.position,_tr.position) > 25f) return;
+        return;
+        if(Vector3.Distance(_player.transform.position,_tr.position) > 50f) return;
+        
         if(_enabled)_particleCount++;
         _count++;
         if (_enabled && _particleCount >= Mathf.Floor(60f / particleFrequency))
